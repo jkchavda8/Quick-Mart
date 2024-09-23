be5427e525769a08ea7a0791ac0310ff28dd38d7
MeetGJethva <157043580+MeetGJethva@users.noreply.github.com>
2024-09-23 22:59:29 +0530

    Update Splash_screen.dart
    change startapp function

--- lib/components/Splash_screen.dart
+++ lib/components/Splash_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:quickmartfinal/services/UserServices.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -13,6 +15,15 @@ class _SplashScreenState extends State<SplashScreen> {
   startApp() {
     Timer(Duration(seconds: 1), () async {
       // Navigate to the login page using named routes
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      bool? isLoggedIn = prefs.getBool('isLoggedIn');
+      if(isLoggedIn!){
+        var email = prefs.getString('email');
+        var password = prefs.getString('password');
+        if(email != '' && password != ''){
+          await UserService().loginUser(email!, password!);
+        }
+      }
       Navigator.pushReplacementNamed(context, '/home');
     });
   }
