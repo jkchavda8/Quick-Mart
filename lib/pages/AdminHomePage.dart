9e822053a52db133509a77e767bde5dcf462f937
Nikunj <nikunjganvit223@gmail.com>
2024-10-16 22:30:04 +0530

    Chnages in Admin Login and Admin Home Pages.

--- lib/pages/AdminHomePage.dart
+++ lib/pages/AdminHomePage.dart
@@ -8,55 +8,108 @@ class AdminHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Admin Product Management'),
+        title: const Text(
+          'Admin Product Management',
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        backgroundColor: Colors.deepPurpleAccent,
       ),
-      body: StreamBuilder<List<Map<String, dynamic>>>(
-        stream: _productService.fetchAllProducts(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(child: CircularProgressIndicator());
-          }
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: StreamBuilder<List<Map<String, dynamic>>>(
+          stream: _productService.fetchAllProducts(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            }
 
-          List<Map<String, dynamic>> products = snapshot.data!;
+            List<Map<String, dynamic>> products = snapshot.data!;
 
-          return ListView.builder(
-            itemCount: products.length,
-            itemBuilder: (context, index) {
-              final product = products[index];
-              final productId = product['product_id'];
-              final isApproved = product['isApproved'] ?? false;
+            return ListView.builder(
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                final product = products[index];
+                final productId = product['product_id'];
+                final isApproved = product['isApproved'] ?? false;
 
-              return ListTile(
-                title: Text(product['name']),
-                subtitle: Text('Price: \$${product['price']}'),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    if (!isApproved) // Show Approve button if not approved
-                      ElevatedButton(
-                        onPressed: () => _productService.approveProduct(productId),
-                        child: const Text('Approve'),
-                      ),
-                    if (isApproved) // Show Block or Unblock button based on isApproved status
-                      ElevatedButton(
-                        onPressed: () {
-                          if (isApproved) {
-                            _productService.blockProduct(productId);
-                          } else {
-                            _productService.unblockProduct(productId);
-                          }
-                        },
-                        child: Text(isApproved ? 'Block' : 'Unblock'),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: isApproved ? Colors.red : Colors.green,
+                return Card(
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  elevation: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          product['name'],
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
                         ),
-                      ),
-                  ],
-                ),
-              );
-            },
-          );
-        },
+                        const SizedBox(height: 8),
+                        Text(
+                          'Price: \$${product['price']}',
+                          style: TextStyle(fontSize: 16, color: Colors.grey[700]),
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            if (!isApproved) // Approve button for unapproved products
+                              ElevatedButton(
+                                onPressed: () =>
+                                    _productService.approveProduct(productId),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.green,
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 20, vertical: 12),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                                child: const Text(
+                                  'Approve',
+                                  style: TextStyle(fontSize: 16, color: Colors.white),
+                                ),
+                              ),
+                            if (isApproved) // Block/Unblock button for approved products
+                              ElevatedButton(
+                                onPressed: () {
+                                  if (isApproved) {
+                                    _productService.blockProduct(productId);
+                                  } else {
+                                    _productService.unblockProduct(productId);
+                                  }
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                  isApproved ? Colors.red : Colors.green,
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 20, vertical: 12),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                                child: Text(
+                                  isApproved ? 'Block' : 'Unblock',
+                                  style: const TextStyle(fontSize: 16,color: Colors.white ),
+                                ),
+                              ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
--- lib/pages/AdminLoginPage.dart
+++ lib/pages/AdminLoginPage.dart
@@ -32,37 +32,116 @@ class _AdminLoginPageState extends State<AdminLoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Admin Login'),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.close, color: Colors.deepPurpleAccent),
+            onPressed: () {
+              Navigator.pushNamed(context, '/home');
+            },
+          ),
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                hintText: 'Enter your email',
-                errorText: errorMessage != null ? errorMessage : null,
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(
+                Icons.admin_panel_settings_outlined,
+                size: 100,
+                color: Colors.deepPurpleAccent,
               ),
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                hintText: 'Enter your password',
-                errorText: errorMessage != null ? errorMessage : null,
+              const SizedBox(height: 20),
+              Text(
+                'Admin Login',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepPurpleAccent,
+                ),
               ),
-            ),
-            const SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: _login,
-              child: const Text('Login'),
-            ),
-          ],
+              const SizedBox(height: 40),
+              Card(
+                elevation: 6,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: _emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          hintText: 'Enter admin email',
+                          prefixIcon: Icon(Icons.email_outlined, color: Colors.deepPurpleAccent),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.deepPurpleAccent),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      TextField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          hintText: 'Enter password',
+                          prefixIcon: Icon(Icons.lock_outline, color: Colors.deepPurpleAccent),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.deepPurpleAccent),
+                          ),
+                        ),
+                      ),
+                      if (errorMessage != null) ...[
+                        const SizedBox(height: 10),
+                        Text(
+                          errorMessage!,
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: _login,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.deepPurpleAccent, // Button background color
+                          padding: const EdgeInsets.symmetric(vertical: 15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          minimumSize: const Size(double.infinity, 50),
+                        ),
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white, // Set the text color to white
+                          ),
+                        ),
+                      ),
+
+
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
